64facbe0c424051d0d485cbcfeabadb182355ae3
gstolaric <geoffroy.stolaric@maximo.fr>
2020-11-04 13:45:12 +0100

    init projet

--- hello.js
+++ hello.js
@@ -1,10 +1,19 @@
 var http = require('http');
-var test  = "coucou";
+
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var test  = "coucou 5";
 var hello = "Hello World\n";
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(test + " " + hello);
-}).listen(1337, '127.0.0.1');
+
+app.get('/',function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+   	res.end(test + " " + hello);
+});
+
+
+app.listen(1337);
+
 
 console.log('Server running at http://127.0.0.1:1337/');
-
