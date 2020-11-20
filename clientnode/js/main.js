cc49be275e37dd4aa9434222f88f2ae80502511c
gstolaric <geoffroy.stolaric@maximo.fr>
2020-11-20 11:25:12 +0100

    comportement OK ajout de redirection a la deco

--- clientnode/js/main.js
+++ clientnode/js/main.js
@@ -9,7 +9,7 @@ var mainScript = function(){
     this.gauche = document.querySelectorAll('div.gauche');
     this.main = document.querySelectorAll('div.main');
     this.login =  document.getElementById("login");
-
+    this.idClientConnected = null;
     this.socketid = null;
 };
 
@@ -32,20 +32,21 @@ mainScript.prototype.setElementVisibility = function(element,visible){
 
 mainScript.prototype.init = function(){
     let that = this;
+    that.setElementVisibility(that.main[0].querySelectorAll('div.deconnect')[0], false);
     that.bindLoginSubmit();
+    that.registerToNodeJsServer()
+	.then((okConnectSocketIO) => { 
+	});						    
 };    
 
 mainScript.prototype.registerToNodeJsServer = function() {
         let that = this;
         var res = new Promise(function (resolve, reject) {
-
             
-                that.nodeJSclient = new nodeJSclient();
-                // Connexion au serveur
-                resolve(that.nodeJSclient.connectServer());
-                
-                reject("connection nodejs serveur impossible");
-            
+            that.nodeJSclient = new nodeJSclient();
+            // Connexion au serveur
+            resolve(that.nodeJSclient.connectServer(that.idClientConnected));                
+            reject("connection nodejs serveur impossible");          
 
         });
 
@@ -54,131 +55,128 @@ mainScript.prototype.registerToNodeJsServer = function() {
 
 mainScript.prototype.affichage = function(idClient=null, datas={}, statut="off"){
         
-        let that = this;
-        if(statut === "on"){
-            if(that.main[0].querySelectorAll('span')[0]) {
-            
-            } else {
-                var span = document.createElement("SPAN");
-                that.main[0].appendChild(span).innerHTML = "";
-                that.main[0].querySelectorAll('span')[0].innerHTML = datas.client.infos.nom;
-                
-
-                var input  = document.createElement("input");
-                that.main[0].appendChild(input).setAttribute("id","client_id");
-                that.main[0].appendChild(input).setAttribute("type","text");                      
-                document.getElementById("client_id").value =datas.client.id;
-
-                var div = document.createElement("DIV");
-                that.resultat[0].appendChild(div).innerHTML=datas.client.id + " " + datas.client.infos.nom + " " + datas.client.infos.role + " connecté"; 
-                
-                that.setElementVisibility(that.main[0].querySelectorAll('div.deconnect')[0], true);
-                that.setElementVisibility(that.main[0].querySelectorAll('div.login')[0], false);
-                
-            }
+    let that = this;
+    if(statut === "on"){
+        if(that.main[0].querySelectorAll('span')[0]) {
+        
         } else {
-                
-            //chercher en fonction de idClient les infos du client qui se deconnecte pour affichage     
-            that.GetInfosClient(idClient)
-            .then((infosClientDeco) =>  {      
-                let nomClientDeco = infosClientDeco.results.nom;
-                var div = document.createElement("DIV");
-                that.resultat[0].appendChild(div).innerHTML=nomClientDeco + " est déconnecté";  
-                // pour gérer l'interface du client concerné
-                if(document.getElementById("client_id")){
-                    if(idClient === parseInt(document.getElementById("client_id").value)){
-                        that.main[0].querySelectorAll('span')[0].remove();
-                        document.getElementById("client_id").remove();
-                        that.resultat[0].innerHTML="";
-                        that.setElementVisibility(that.main[0].querySelectorAll('div.deconnect')[0], false);
-                        that.setElementVisibility(that.main[0].querySelectorAll('div.login')[0], true);
-                    }
-                }
-            });
+            var span = document.createElement("SPAN");
+            that.main[0].appendChild(span).innerHTML = "";
+            that.main[0].querySelectorAll('span')[0].innerHTML = datas.client.infos.nom;
+            
 
+            var input  = document.createElement("input");
+            that.main[0].appendChild(input).setAttribute("id","client_id");
+            that.main[0].appendChild(input).setAttribute("type","text");                      
+            document.getElementById("client_id").value =datas.client.id;
+
+            var div = document.createElement("DIV");
+            that.resultat[0].appendChild(div).innerHTML=datas.client.id + " " + datas.client.infos.nom + " " + datas.client.infos.role + " connecté"; 
             
-            
+            that.setElementVisibility(that.main[0].querySelectorAll('div.deconnect')[0], true);
+            that.setElementVisibility(that.main[0].querySelectorAll('div.login')[0], false);
             
         }
-        
+    } else {
+         
+        //chercher en fonction de idClient les infos du client qui se deconnecte pour affichage     
+        that.GetInfosClient(idClient)
+        .then((infosClientDeco) =>  {      
+            let nomClientDeco = infosClientDeco.results.nom;
+            var div = document.createElement("DIV");
+            that.resultat[0].appendChild(div).innerHTML=nomClientDeco + " est déconnecté";  
+            // pour gérer l'interface du client concerné
+            if(document.getElementById("client_id")){
+                if(idClient === parseInt(document.getElementById("client_id").value)){
+                    that.main[0].querySelectorAll('span')[0].remove();
+                    document.getElementById("client_id").remove();
+                    that.resultat[0].innerHTML="";
+                    that.setElementVisibility(that.main[0].querySelectorAll('div.deconnect')[0], false);
+                    that.setElementVisibility(that.main[0].querySelectorAll('div.login')[0], true);
+                    window.location.href = "/nodeAppli/clientnode/";
+                }
+            }
+        });
+    }       
 };
     
 mainScript.prototype.bindLoginSubmit = function(){
     let that = this;
     let donnees = null;
      
-    if(submit !== undefined){
+    if(submit !== undefined){               
+        if(submit !== undefined){ 
+            submit.addEventListener('click', function(e){
+                e.stopPropagation();
+                e.preventDefault(); 
+                that.SetLoginSession(that.login.value)
+                .then((datasBdd) =>  {   
+                    donnees = datasBdd;
+                    that.idClientConnected = donnees.client.id;
+                    return  that.SetStatutClient(donnees.client.id, "1");
+                })
+                .then((updateStatutClient) => {    
+                    if( updateStatutClient.updateOnline === "1"){          
+						      
+						that.nodeJSclient.socketio.emit('connecton',  donnees);
+
+						that.nodeJSclient.socketio.on('connecton', function(User){
+
+						    that.affichage(null,donnees, "on");
+						    //ici a chaque fois qu'un client se connecte                            
+						    var sortie = User.data.client.id + " " + User.data.client.infos.nom + " " + User.socketIdClient;
+						    var div = document.createElement("DIV");
+						    that.resultat[0].appendChild(div).innerHTML=sortie + " ONLINE";
+						    
+
+						});
+						if(deco !== undefined){
+						    deco.addEventListener('click', function(e){
+						        e.stopPropagation();
+						        e.preventDefault(); 
+						        var clientId = donnees.client.id
+						        compteur=0;
+						        that.resultat[0].innerHTML="";
+						        that.nodeJSclient.socketio.emit('coupe',  clientId);            
+						        return false;                           
+						    });
+						    that.nodeJSclient.socketio.on('coupe', function( socketId, clientId){
+
+						        that.SetStatutClient(clientId, "0")
+						        .then((update) => {
+						            that.affichage(clientId, {}, "off");
+
+						            
+						        });
+
+						    }); 
+						}
+						if(lien !== undefined){
+						    lien.addEventListener('click', function(e){
+						        e.stopPropagation();
+						        e.preventDefault(); 
+						        var client = donnees.client;
+						        that.nodeJSclient.socketio.emit('clicklien', "ok", client);
+						        compteur++;
+
+						   		console.log(compteur);
+						        return false;
+						    });       
+						    that.nodeJSclient.socketio.on('clicklien', function(msg, client, socketID){                  
+						        var div = document.createElement("DIV");
+						        that.resultat[0].appendChild(div).innerHTML= socketID + " "  + msg + " id: " +  client.id + "nom: " + client.infos.nom; 
+						    });     
+						}
+
+													                    
+                    }
+                });
+                return false;                           
+            });
+        }
         
-        that.registerToNodeJsServer()         
-        .then((okConnectSocketIO) => {   
-            if(submit !== undefined){ 
-                submit.addEventListener('click', function(e){
-                    e.stopPropagation();
-                    e.preventDefault(); 
-                    that.SetLoginSession(that.login.value)
-                    .then((datasBdd) =>  {   
-                        donnees = datasBdd;
-                        return  that.SetStatutClient(donnees.client.id, "1");
-                    })
-                    .then((updateStatutClient) => {    
-                        if( updateStatutClient.updateOnline === "1"){
 
-                            that.nodeJSclient.socketio.emit('connecton',  donnees);
-
-                            that.nodeJSclient.socketio.on('connecton', function(User){
-
-                                that.affichage(null,donnees, "on");
-                                //ici a chaque fois qu'un client se connecte                            
-                                var sortie = User.data.client.id + " " + User.data.client.infos.nom + " " + User.socketIdClient;
-                                var div = document.createElement("DIV");
-                                that.resultat[0].appendChild(div).innerHTML=sortie + " ONLINE";
-                                compteur++;
-
-                                console.log(compteur);
-
-                            });
-
-                        }
-                    });
-                    return false;                           
-                });
-            }
-            if(deco !== undefined){
-                deco.addEventListener('click', function(e){
-                    e.stopPropagation();
-                    e.preventDefault(); 
-                    var clientId = donnees.client.id
-                    compteur=0;
-                    that.resultat[0].innerHTML="";
-                    that.nodeJSclient.socketio.emit('coupe',  clientId);            
-                    return false;                           
-                });
-                that.nodeJSclient.socketio.on('coupe', function( socketId, clientId){
-
-                    that.SetStatutClient(clientId, "0")
-                    .then((update) => {
-                        that.affichage(clientId, {}, "off");
-                        
-                    });
-
-                }); 
-            }
-            if(lien !== undefined){        
-                lien.addEventListener('click', function(e){
-                    e.stopPropagation();
-                    e.preventDefault(); 
-                    var client = donnees.client;
-                    that.nodeJSclient.socketio.emit('clicklien', "ok", client);
-
-                    return false;
-                });       
-                that.nodeJSclient.socketio.on('clicklien', function(msg, client, socketID){                  
-                    var div = document.createElement("DIV");
-                    that.resultat[0].appendChild(div).innerHTML= socketID + " "  + msg + " id: " +  client.id + "nom: " + client.infos.nom; 
-                });     
-            }
-
-        })    
+         
     }
 };
 
